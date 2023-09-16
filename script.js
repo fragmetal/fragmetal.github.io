9ff6eceb0ca96a6ea7690dfd56d2cedb9916d53a
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-09-16 23:05:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeText() {
+  document.getElementById('heading').innerHTML = "You clicked the button!";
+  document.getElementById('paragraph').innerHTML = "The text has now changed.";
+}
