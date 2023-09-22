96774dd810c8a17b977b914130ba3d3562290e47
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-09-22 13:27:48 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function changeText() {
-  document.getElementById('heading').innerHTML = "You clicked the button!";
-  document.getElementById('paragraph').innerHTML = "The text has now changed.";
-}
